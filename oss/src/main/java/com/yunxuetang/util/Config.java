7c14f1a97a26f98cb5e306ad32fa996a81b28d37
xur <xur@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-01-19 10:13:40 +0000

    端口8084

--- oss/src/main/java/com/yunxuetang/util/Config.java
+++ oss/src/main/java/com/yunxuetang/util/Config.java
@@ -5,7 +5,7 @@ public class Config {
 	public static String YXTSERVER = "http://s1.xuewen.yunxuetang.com:8084/";
 	public static String YXTSERVER2 = "http://s1.xuewen.yunxuetang.com:8082/";
 //	public static String YXTSERVER3 = "http://localhost:8084/";
-	public static String YXTSERVER3 = "http://localhost:8080/";
+	public static String YXTSERVER3 = "http://localhost:8084/";
 	// 标签服务
 	public static String YXTSERVER4 = "http://s1.xuewen.yunxuetang.com:8161/";
 }
