dae9d0f3977fea6bd21770c43fa3741326d7363f
donny <donny@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-01-28 12:35:25 +0000

    修正RestTemplate的PostForObject方法调用，兼容之前的做法

--- oss/src/main/java/com/yunxuetang/oss/BaseController.java
+++ oss/src/main/java/com/yunxuetang/oss/BaseController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import net.sf.json.JSONObject;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.yunxuetang.util.Config;
@@ -14,7 +16,11 @@ public class BaseController {
 		// TODO Auto-generated constructor stub
 	}
 
-	@SuppressWarnings("unused")
+	/**
+	 * 传入一个地址，Get 请求
+	 * @param url
+	 * @return
+	 */
 	public JSONObject getRestApiData(String url) {
 		String courSharResoStr;
 		RestTemplate restTemplate = new RestTemplate();
@@ -27,13 +33,39 @@ public class BaseController {
 		}
 	}
 
-	@SuppressWarnings("unused")
+	/**
+	 * 传入Map，Post到对一个的地址（为了兼容之前传入的Map参数）
+	 * @param url
+	 * @param requestParams
+	 * @return JSONObject
+	 */
 	public JSONObject getRestApiData(String url,
 			Map<String, String> requestParams) {
 		String courSharResoStr;
+		
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>(); 
+		map.setAll(requestParams);
+		
 		RestTemplate restTemplate = new RestTemplate();
-		courSharResoStr = restTemplate.postForObject(url, null, String.class,
-				requestParams);
+		courSharResoStr = restTemplate.postForObject(url, map, String.class,"");
+		try {
+			JSONObject objj = JSONObject.fromObject(courSharResoStr);
+			return objj;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	/**
+	 * 传入MultiValueMap，Post给对应的地址
+	 * @param url
+	 * @param requestParams
+	 * @return JSONObject
+	 */
+	public JSONObject getRestApiData(String url,
+			MultiValueMap<String, String> requestParams) {
+		String courSharResoStr;
+		RestTemplate restTemplate = new RestTemplate();
+		courSharResoStr = restTemplate.postForObject(url, requestParams, String.class, "");
 		try {
 			JSONObject objj = JSONObject.fromObject(courSharResoStr);
 			return objj;
