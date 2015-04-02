84bf76e712dc3f67a47039ee3cea49ec3bdd5c83
yangt <yangt@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-04-02 10:48:36 +0000

    修改标签服务地址

--- oss/src/main/java/com/yunxuetang/util/Config.java
+++ oss/src/main/java/com/yunxuetang/util/Config.java
@@ -19,9 +19,14 @@ public class Config {
 	
 	//public static String YXTSERVER3 = "http://s1.xuewen.yunxuetang.com:8084/";
 	// 标签服务
-	public static String YXTSERVER4 = "http://s1.xuewen.yunxuetang.com:8161/";
+	public static String YXTSERVER4 = "http://chat.yunxuetang.com:8161/";
 	public static String WIKERSERVER = "http://s1.xuewen.yunxuetang.com:8089/";
 	public static String QDANSERVER = "http://bj.api.qd.ztiao.cn/";
 	public static String ACCESS_KEY = "PN6SV6Fm-Ueu-D8NYN_NfJwpPuedsQALNKmwL_IQ";
 	public static String SECRET_KEY = "O6sBs8dw9cJjamEx2d6vgY1sXnPnIlg5Nsn6CwPH";
+	
+	/**
+	 * 公共方法中，domain 域的定义
+	 */
+	public final static String YXTDOMAIN="yxt";
 }
