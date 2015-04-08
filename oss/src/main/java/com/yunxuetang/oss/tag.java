6d466a98f200251207f79e6f8046377dd1ef1547
yangt <yangt@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-04-08 06:55:47 +0000

    修正修改标签报错的问题

--- oss/src/main/java/com/yunxuetang/oss/tag.java
+++ oss/src/main/java/com/yunxuetang/oss/tag.java
@@ -9,6 +9,7 @@ import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.yunxuetang.util.Config;
