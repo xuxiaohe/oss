0e803bccfd7d1dbe45bd32ad14a0e9818cc1e6e9
yangt <yangt@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-05-19 06:45:37 +0000

    增加按订单状态查询功能

--- oss/src/main/java/com/yunxuetang/oss/Order.java
+++ oss/src/main/java/com/yunxuetang/oss/Order.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yunxuetang.util.Config;
+import com.yunxuetang.util.StringUtil;
 
 @Controller
 @RequestMapping("/order")
@@ -19,7 +20,14 @@ public class Order extends BaseController{
 	 * */
 	@RequestMapping("list")
 	public String orderList(HttpServletRequest request, Model model){
-		model.addAttribute("orderlist", getOrderList());
+		String status = request.getParameter("orderStatus");
+		if(StringUtil.isEmpty(status)){
+			model.addAttribute("orderlist", getOrderList());
+		}else{
+			model.addAttribute("orderlist", getOrderListByStatus(status));
+			model.addAttribute("orderStatus", status);
+		}
+		//model.addAttribute("orderType", getOrderTypeList());
 		String cpath = request.getContextPath();
 		String cbasePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + cpath + "/";
 		model.addAttribute("cbasePath", cbasePath);
@@ -28,8 +36,13 @@ public class Order extends BaseController{
 	}
 	
 	
+	
 	private JSONObject getOrderList(){
 		String url = Config.ORDER_SERVER + "/order/allOrders";
+		return getRestApiData(url);
+	}
+	private JSONObject getOrderListByStatus(String status){
+		String url = Config.ORDER_SERVER + "/order/findOrdersBystate?orderStatus=" + status;
 		return getRestApiData(url);
 	}
 }
