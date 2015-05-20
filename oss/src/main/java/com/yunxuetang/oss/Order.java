a7aba6d12783e5ea7e89f36ef0f8a9e50060b153
xur <xur@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-05-20 07:22:01 +0000

    bug

--- oss/src/main/java/com/yunxuetang/oss/Order.java
+++ oss/src/main/java/com/yunxuetang/oss/Order.java
@@ -43,12 +43,45 @@ public class Order extends BaseController{
 		return "order/orderlist";
 	}
 	
+	/**
+	 * 根据时间间隔获取订单列表
+	 * */
+	@RequestMapping("findOrder")
+	public String orderBytime(HttpServletRequest request, Model model){
+		String n = request.getParameter("n");
+		String s = request.getParameter("s");
+		
+		String starttime = request.getParameter("starttime");
+		String endtime = request.getParameter("endtime");
+		if(StringUtil.isEmpty(n)){
+			n = "0";
+		}
+		if(StringUtil.isEmpty(s)){
+			s = "10";
+		}
+		if(!("0".equals(starttime))&&!("0".equals(endtime))){
+			model.addAttribute("orderlist", getOrderByTime(n, s,starttime,endtime));
+		}
+		//model.addAttribute("orderType", getOrderTypeList());
+		String cpath = request.getContextPath();
+		String cbasePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + cpath + "/";
+		model.addAttribute("cbasePath", cbasePath);
+		model.addAttribute("sourcePath", Config.YXTSERVER5);
+		return "order/orderlist";
+	}
+	
 	
 	
 	private JSONObject getOrderList(String n, String s){
 		String url = Config.ORDER_SERVER + "/order/allOrders?n=" + n + "&s=" + s;
 		return getRestApiData(url);
 	}
+	
+	private JSONObject getOrderByTime(String n, String s,String starttime, String endtime){
+		String url = Config.ORDER_SERVER + "/order/allOrders?n=" + n + "&s=" + s+ "&starttime=" + starttime+ "&endtime=" + endtime;
+		return getRestApiData(url);
+	} 
+	
 	private JSONObject getOrderListByStatus(String status, String n, String s){
 		String url = Config.ORDER_SERVER + "/order/findOrdersBystate?n=" + n + "&s=" + s + "&orderStatus=" + status;
 		return getRestApiData(url);
