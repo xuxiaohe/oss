1b409df54b575c77fadd0573ee900fe4481acc59
yangt <yangt@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-05-28 06:13:35 +0000

    新增发送系统消息功能

--- oss/src/main/java/com/yunxuetang/oss/message.java
+++ oss/src/main/java/com/yunxuetang/oss/message.java
@@ -10,6 +10,7 @@ import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yunxuetang.util.Config;
 import com.yunxuetang.util.StringUtil;
@@ -17,23 +18,32 @@ import com.yunxuetang.util.StringUtil;
 @Controller
 @RequestMapping("/message")
 public class message extends BaseController{
+	
+	
+	@RequestMapping("sendView")
+	public String sendView(HttpServletRequest request, Model model){
+		String cpath = request.getContextPath();
+		String cbasePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + cpath + "/";
+		model.addAttribute("cbasePath", cbasePath);
+		model.addAttribute("sourcePath", Config.YXTSERVER5);
+		return "message/send";
+	}
 
 	/**
 	 * 发送消息
 	 * */
 	@RequestMapping("sendmessage")
-	public String orderList(HttpServletRequest request, Model model){
-		String status = request.getParameter("orderStatus");
+	public @ResponseBody JSONObject orderList(HttpServletRequest request, Model model){
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
 		
-		sendmessage(title, content);
+		JSONObject result = sendmessage(title, content);
 		//model.addAttribute("orderType", getOrderTypeList());
-		String cpath = request.getContextPath();
-		String cbasePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + cpath + "/";
-		model.addAttribute("cbasePath", cbasePath);
-		model.addAttribute("sourcePath", Config.YXTSERVER5);
-		return "message/orderlist";
+//		String cpath = request.getContextPath();
+//		String cbasePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + cpath + "/";
+//		model.addAttribute("cbasePath", cbasePath);
+//		model.addAttribute("sourcePath", Config.YXTSERVER5);
+		return result;
 	}
 	
 	
