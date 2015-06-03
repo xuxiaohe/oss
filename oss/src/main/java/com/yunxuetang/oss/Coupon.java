ea00683cb411ab6138aabcb42c283e994585717a
yangt <yangt@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-06-03 04:52:07 +0000

    修正页面重定向的BUG

--- oss/src/main/java/com/yunxuetang/oss/Coupon.java
+++ oss/src/main/java/com/yunxuetang/oss/Coupon.java
@@ -97,7 +97,7 @@ public class Coupon extends BaseController{
 				+ cpath + "/";
 		model.addAttribute("cbasePath", cbasePath);
 		model.addAttribute("sourcePath", Config.YXTSERVER5);
-		return "redirect:coupon/couponList";
+		return "redirect:/coupon/couponList";
 	}
 	
 	/**
