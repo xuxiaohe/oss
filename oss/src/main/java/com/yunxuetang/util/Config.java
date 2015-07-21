93216050796b315a7124d028b1f25973d193d931
xur <xur@cc609152-ec80-4754-a5f7-4faf359339e8>
2015-07-21 10:18:34 +0000

    oss

--- oss/src/main/java/com/yunxuetang/util/Config.java
+++ oss/src/main/java/com/yunxuetang/util/Config.java
@@ -19,9 +19,9 @@ public class Config {
 	
 	//public static String YXTSERVER3 = "http://s1.xuewen.yunxuetang.com:8084/";
 	// 标签服务 开发
-	public static String YXTSERVER4 = "http://chat.yunxuetang.com:8161/";
+	//public static String YXTSERVER4 = "http://chat.yunxuetang.com:8161/";
 	//生产
-	//public static String YXTSERVER4 = "http://bj.tag.ztiao.cn/";
+	public static String YXTSERVER4 = "http://bj.tag.ztiao.cn/";
 	
 	public static String WIKERSERVER = "http://bj.api.wiker.ztiao.cn/";
 	public static String QDANSERVER = "http://bj.api.qd.ztiao.cn/";
@@ -51,7 +51,7 @@ public class Config {
 	// 专题盒子服务 本地开发环境
 	// public static String MAHOUT_SERVER = "http://localhost:8324";
 	// 专题盒子服务 chat开发环境
-	// public static String MAHOUT_SERVER = "http://chat1.yunxuetang.com:8324";
+	//public static String MAHOUT_SERVER = "http://chat1.yunxuetang.com:8324";
 	// 专题盒子服务 qingyun生产环境
 	public static String MAHOUT_SERVER = "http://192.168.100.5:8391";
 	
